880e8eb0473a3391e7879aa991644296ffadb0b3
Qeuis <dyu5520@gmail.com>
2021-10-02 22:56:25 -0400

    added pseudocode skeleton

---
+++ Exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,39 @@
+package baseline;
+
+public class Solution24
+{
+    public static void main (String [] args)
+    {
+      /*
+      main method where anagram program will execute
+       */
+    }
+
+    public void getString()
+    {
+        /*
+        method where user is prompted for string input
+         */
+    }
+
+    public void returnString()
+    {
+        /*
+        returns string
+         */
+    }
+
+    public void checksIfAnagram()
+    {
+        /*
+        checks if user string inputs are anagrams
+         */
+    }
+
+    public void returnIfAnagram()
+    {
+        /*
+        returns if user string inputs are anagrams or not
+         */
+    }
+}
